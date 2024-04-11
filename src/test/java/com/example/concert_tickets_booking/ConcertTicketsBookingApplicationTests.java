f81e8cd891155204b6d39f6119067344955f7642
YevheniiKyr <jekafekz@gmail.com>
2024-04-11 19:27:00 +0300

    basic project structure

---
+++ src/test/java/com/example/concert_tickets_booking/ConcertTicketsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concert_tickets_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketsBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
